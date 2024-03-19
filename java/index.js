882b6d987ac047128bde027bcba935773942c19a
Yahia Lakrikba <161433620+YahiaLakrikba2004@users.noreply.github.com>
2024-03-19 12:08:57 +0100

    color congratulation phrase

--- java/index.js
+++ java/index.js
@@ -1,5 +1,5 @@
 const totalQuestions = 10
-const correctAnswers = 8
+const correctAnswers = 6.7
 
 const correctPercentage = (correctAnswers / totalQuestions) * 100
 const wrongPercentage = 100 - correctPercentage
@@ -16,3 +16,5 @@ function redirectToNewPage() {
 
   window.location.href = newPageUrl
 }
+
+document.getElementById('frase').style.color = 'cyan'
