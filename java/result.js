ee921a29f5f9e64e1f6892bbe12753be519175f3
ABBA3124 <davideabbadessa73@gmail.com>
2024-03-19 13:39:39 +0100

    dcscds
    cvdc

--- java/result.js
+++ java/result.js
@@ -1,20 +1,20 @@
-const totalQuestions = 10
-const correctAnswers = 2.5
+// const totalQuestions = 10
+// const correctAnswers = 2.5
 
-const correctPercentage = (correctAnswers / totalQuestions) * 100
-const wrongPercentage = 100 - correctPercentage
+// const correctPercentage = (correctAnswers / totalQuestions) * 100
+// const wrongPercentage = 100 - correctPercentage
 
-document.getElementById('correctPercentage').innerText = `${correctPercentage}%`
-document.getElementById('wrongPercentage').innerText = `${wrongPercentage}%`
+// document.getElementById('correctPercentage').innerText = `${correctPercentage}%`
+// document.getElementById('wrongPercentage').innerText = `${wrongPercentage}%`
 
-const circle = document.querySelector('.progress-circle')
-const circleAngle = (correctPercentage / 100) * 360
-circle.style.backgroundImage = `conic-gradient(#00ffff ${circleAngle}deg, #c2128d 0%)`
+// const circle = document.querySelector('.progress-circle')
+// const circleAngle = (correctPercentage / 100) * 360
+// circle.style.backgroundImage = `conic-gradient(#00ffff ${circleAngle}deg, #c2128d 0%)`
 
-function redirectToNewPage() {
-  let newPageUrl = "feedbak.html"
+// function redirectToNewPage() {
+//   let newPageUrl = "feedbak.html"
 
-  window.location.href = newPageUrl
-}
+//   window.location.href = newPageUrl
+// }
 
-document.getElementById('frase').style.color = 'cyan'
+// document.getElementById('frase').style.color = 'cyan'
