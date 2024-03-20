82825aac7fd44c2e963676f7ed5d40b1e7aee892
MARINA MARIA LUCENTINI <158228930+MarinaLucentini@users.noreply.github.com>
2024-03-20 17:29:31 +0100

    grafic

--- java/result.js
+++ java/result.js
@@ -1,7 +1,6 @@
 // Recupera l'array results dai dati di sessione
-const results = JSON.parse(sessionStorage.getItem('results')) || [];
-console.log("log di come arriva array per controllo",results); // Per controllo
-
+const results = JSON.parse(sessionStorage.getItem("results")) || [];
+console.log("log di come arriva array per controllo", results); // Per controllo
 
 // Calcola risposte giuste e sbagliate
 let risposteGiuste = 0;
@@ -16,41 +15,89 @@ results.forEach((result) => {
     risposteSbagliate += 1;
   }
 });
-console.log("log una volta eseguiti i calcoli mi accerto che arrivi",results)
-console.log("log una volta eseguiti i calcoli mi accerto che arrivi risposte giuste",risposteGiuste)
-console.log("log una volta eseguiti i calcoli mi accerto che arrivi risposte sbagliate",risposteSbagliate)
-
+console.log("log una volta eseguiti i calcoli mi accerto che arrivi", results);
+console.log(
+  "log una volta eseguiti i calcoli mi accerto che arrivi risposte giuste",
+  risposteGiuste
+);
+console.log(
+  "log una volta eseguiti i calcoli mi accerto che arrivi risposte sbagliate",
+  risposteSbagliate
+);
 
 // Calcolo totale delle domande
 const totaleDomande = results.length;
-console.log("mi accerto il numero totale delle domande",totaleDomande)
+console.log("mi accerto il numero totale delle domande", totaleDomande);
 
 // Calcola le percentuali
 const percentualeCorrette = ((risposteGiuste / totaleDomande) * 100).toFixed(2);
-const percentualeSbagliate = ((risposteSbagliate / totaleDomande) * 100).toFixed(2);
+const percentualeSbagliate = (
+  (risposteSbagliate / totaleDomande) *
+  100
+).toFixed(2);
 
-console.log("percentuale corrette",percentualeCorrette,"%")
-console.log("percentuale sbagliata",percentualeSbagliate,"%")
+console.log("percentuale corrette", percentualeCorrette, "%");
+console.log("percentuale sbagliata", percentualeSbagliate, "%");
 
-
-//mette in atto tutti i calcoli eseguiti e inserisce i vari campi al suo posto 
-document.getElementsByClassName('risultatiCorretti')[0].innerHTML = `<h6><span class="correct">Correct</span><br/><span class="percentuale">${percentualeCorrette}%</span><br/><span class="totSuTot">${risposteGiuste}/${totaleDomande} Questions</span></h6>`;
-document.getElementsByClassName('risultatiSbagliati')[0].innerHTML = `<h6><span class="correct">Wrong</span><br/><span class="percentuale">${percentualeSbagliate}%</span><br/><span class="totSuTot">${risposteSbagliate}/${totaleDomande} Questions</span></h6>`;
+//mette in atto tutti i calcoli eseguiti e inserisce i vari campi al suo posto
+document.getElementsByClassName(
+  "risultatiCorretti"
+)[0].innerHTML = `<h6><span class="correct">Correct</span><br/><span class="percentuale">${percentualeCorrette}%</span><br/><span class="totSuTot">${risposteGiuste}/${totaleDomande} Questions</span></h6>`;
+document.getElementsByClassName(
+  "risultatiSbagliati"
+)[0].innerHTML = `<h6><span class="correct">Wrong</span><br/><span class="percentuale">${percentualeSbagliate}%</span><br/><span class="totSuTot">${risposteSbagliate}/${totaleDomande} Questions</span></h6>`;
 
 // Determina il messaggio in base alla percentuale di risposte corrette
-const messaggioEsito = percentualeCorrette >= 60 ? "Congratulations!" : "I am sorry";
-const messaggioEsito2 = percentualeCorrette >= 60 ? "You passed the exam." : "You failed the test.";
-//prove
-console.log(messaggioEsito)
-console.log(messaggioEsito2)
+// const messaggioEsito =
+//   percentualeCorrette >= 60 ? "Congratulations!" : "I am sorry";
+// const messaggioEsito2 =
+//   percentualeCorrette >= 60 ? "You passed the exam." : "You failed the test.";
+// //prove
+// console.log(messaggioEsito);
+// console.log(messaggioEsito2);
 
-// Aggiorna il documento HTML con il messaggio di esito
-document.getElementsByClassName('testoInternoGrafico')[0].innerHTML = `<h3>${messaggioEsito}</h3></br><h4>${messaggioEsito2}</h4><h5>We'll send you the certificate<br />in few minutes.<br />
-check your email (including<br />promotions/ spam folder)</h5>`
+// // Aggiorna il documento HTML con il messaggio di esito
+// document.getElementsByClassName(
+//   "testoInternoGrafico"
+// )[0].innerHTML = `<h3>${messaggioEsito}</h3></br><h4>${messaggioEsito2}</h4><h5>We'll send you the certificate<br />in few minutes.<br />
+// check your email (including<br />promotions/ spam folder)</h5>`;
 
 //reazione al click che ti porta alla pagina feedback
 const button = document.querySelector(".bottoneRateUs");
-button.addEventListener("click", function() {
-  window.location.href = "feedback.html"; 
+button.addEventListener("click", function () {
+  window.location.href = "feedback.html";
   sessionStorage.setItem("refreshIndexPage", "true");
-})
+});
+const grafic = () => {
+  const passedOrNot = document.getElementById("passed");
+  const passedOrNot2 = document.getElementById("passed2");
+  const graficResult = document.getElementById("grafic");
+  graficResult.classList.add("grafic");
+  if (percentualeCorrette >= 60) {
+    passedOrNot.innerText = "Congratulations!";
+    passedOrNot2.innerText = "You passed the exam.";
+  } else {
+    passedOrNot.innerText = "I am sorry";
+    passedOrNot2.innerText = "You failed the test.";
+  }
+  graficResult.style.background = `conic-gradient(red ${percentualeSbagliate}%, green 0%)`;
+};
+grafic();
+const list = () => {
+  const listAnswer = document.getElementById("list");
+  for (let i = 0; i < results.length; i++) {
+    const answers = document.createElement("li");
+    listAnswer.appendChild(answers);
+    answers.innerText =
+      "Question" +
+      " " +
+      results[i].question +
+      "Selected answer" +
+      " " +
+      results[i].selectedAnswer +
+      "Correct answer" +
+      " " +
+      results[i].correctAnswer;
+  }
+};
+list();
