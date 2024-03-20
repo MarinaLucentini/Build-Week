8c2d2c999bc8f7cdbe86fb0f820dd952abcc88c1
MARINA MARIA LUCENTINI <158228930+MarinaLucentini@users.noreply.github.com>
2024-03-20 09:01:02 +0100

    add circle

--- java/benchmark.js
+++ java/benchmark.js
@@ -143,27 +143,50 @@ nextQuestion();
 // timer
 let counter = 60;
 let progress = 0;
+const timercontainer = () => {
+  const timercontainerHeader = document.querySelector("header");
+  const timerbar = document.createElement("div");
+  timercontainerHeader.appendChild(timerbar);
+  timerbar.classList.add("external-circle");
+  timerbar.id = "progress-bar";
+  const h4 = document.createElement("h4");
+  const second = document.createElement("p");
+  const rimanenti = document.createElement("p");
+  const textContainer = document.createElement("div");
+  timerbar.appendChild(textContainer);
+  textContainer.classList.add("text-circle");
+  h4.classList.add("progress-textH4");
+  second.classList.add("progress-textP");
+  rimanenti.classList.add("progress-textP");
+  second.innerText = "SECONDS";
+  rimanenti.innerText = "REMAINING";
+  textContainer.append(second, h4, rimanenti);
+  const timer = setInterval(() => {
+    counter--;
+    h4.innerText = " ";
+    h4.innerText = counter;
+    //   questo serve per il countdown dei secondi
+    const progressBar = document.getElementById("progress-bar");
+    progress = progress + 2;
+    progressBar.style.background = `conic-gradient(cyan ${progress}%, #9b9898 0%)`;
 
-const h4 = document.createElement("h4");
-const containerText = document.getElementById("container-text");
-const second = document.createElement("p");
-const rimanenti = document.createElement("p");
-containerText.append(second, h4, rimanenti);
-h4.classList.add("progress-textH4");
-second.classList.add("progress-textP");
-rimanenti.classList.add("progress-textP");
-second.innerText = "SECONDS";
-rimanenti.innerText = "REMAINING";
-const timer = setInterval(() => {
-  counter--;
-  h4.innerText = " ";
-  h4.innerText = counter;
-  //   questo serve per il countdown dei secondi
-  const progressBar = document.getElementById("progress-bar");
-  progress = progress + 2;
-  progressBar.style.background = `conic-gradient(cyan ${progress}%, #9b9898 0%)`;
-
-  if (counter === 0) {
-    clearInterval(timer);
-  }
-}, 900);
+    if (counter === 0) {
+      clearInterval(timer);
+    }
+  }, 900);
+};
+// const h4 = document.createElement("h4");
+// const containerText = document.getElementById("container-text");
+// const second = document.createElement("p");
+// const rimanenti = document.createElement("p");
+// containerText.append(second, h4, rimanenti);
+// h4.classList.add("progress-textH4");
+// second.classList.add("progress-textP");
+// rimanenti.classList.add("progress-textP");
+// second.innerText = "SECONDS";
+// rimanenti.innerText = "REMAINING";
+// const timerbar = document.createElement("div");
+// containerText.appendChild(timerbar);
+// timerbar.classList.add("external-circle");
+// timerbar.id = "progress-bar";
+timercontainer();
