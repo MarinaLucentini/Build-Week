9d0990cfb0243af788a695ac5b8e1b592d85d4f9
MARINA MARIA LUCENTINI <158228930+MarinaLucentini@users.noreply.github.com>
2024-03-21 20:53:21 +0100

    fixed mouse hover

--- java/benchmark.js
+++ java/benchmark.js
@@ -91,124 +91,127 @@ const questions = [
     incorrect_answers: ["512", "1024", "500"],
   },
   // Altre domande...
-]
+];
 
 // Array per salvare l'esito di ogni risposta
-const results = []
+const results = [];
 
 // Codice per il timer
-let counter = 60
-let progress = -19
-let timer
+let counter = 10;
+let progress = -19;
+let timer;
 
 // Definizione dell'elemento h4 e inizializzazione del timer
-const h4 = document.createElement("h4")
-const containerText = document.getElementById("container-text")
-const second = document.createElement("p")
-const rimanenti = document.createElement("p")
-containerText.append(second, h4, rimanenti)
-h4.classList.add("progress-textH4")
-second.classList.add("progress-textP")
-rimanenti.classList.add("progress-textP")
-second.innerText = "SECONDS"
-rimanenti.innerText = "REMAINING"
+const h4 = document.createElement("h4");
+const containerText = document.getElementById("container-text");
+const second = document.createElement("p");
+const rimanenti = document.createElement("p");
+containerText.append(second, h4, rimanenti);
+h4.classList.add("progress-textH4");
+second.classList.add("progress-textP");
+rimanenti.classList.add("progress-textP");
+second.innerText = "SECONDS";
+rimanenti.innerText = "REMAINING";
 
 // Funzione per avviare il timer
 function startTimer() {
-  clearInterval(timer)
-  counter = 50
-  progress = 0
+  clearInterval(timer);
+  counter = 50;
+  progress = 0;
   timer = setInterval(() => {
-    counter--
-    h4.innerText = counter
-    const progressBar = document.getElementById("progress-bar")
-    progress = progress + 2
-    progressBar.style.background = `conic-gradient(cyan ${progress}%, #9b9898 0%)`
+    counter--;
+    h4.innerText = counter;
+    const progressBar = document.getElementById("progress-bar");
+    progress = progress + 2;
+    progressBar.style.background = `conic-gradient(cyan ${progress}%, #9b9898 0%)`;
 
     if (counter === 0) {
-      clearInterval(timer)
-      console.log("Tempo scaduto!")
-      index++
+      clearInterval(timer);
+      console.log("Tempo scaduto!");
+      index++;
       if (index < questions.length) {
-        nextQuestion()
-        startTimer()
+        nextQuestion();
+        startTimer();
       } else {
-        console.log("Hai completato tutte le domande!")
+        console.log("Hai completato tutte le domande!");
       }
     }
-  }, 1000)
+  }, 1000);
 }
 
 // Variabile per tenere traccia dell'indice della domanda corrente
-let index = 0
+let index = 0;
 
 // Funzione per mostrare la prossima domanda
 function nextQuestion() {
-  const contatoreElement = document.querySelector(".valoreContatore")
-  const questionElement = document.querySelector(".Question")
-  const risposteElement = document.querySelector(".contenitoreRisposte")
-  risposteElement.innerHTML = ""
+  const contatoreElement = document.querySelector(".valoreContatore");
+  const questionElement = document.querySelector(".Question");
+  const risposteElement = document.querySelector(".contenitoreRisposte");
+  risposteElement.innerHTML = "";
   // Controlla se siamo all'ultima domanda
   if (index === questions.length) {
     // Se siamo all'ultima domanda, ferma il timer
-    clearInterval(timer)
+    clearInterval(timer);
   } else {
     // Se non siamo all'ultima domanda, avvia il timer per la prossima domanda
-    startTimer()
+    startTimer();
   }
-  const domanda = questions[index]
-  questionElement.innerHTML = `<h1>${domanda.question}</h1>`
-  contatoreElement.textContent = `${index + 1}`
-  const tutteLeRisposte = [...domanda.incorrect_answers, domanda.correct_answer]
-  tutteLeRisposte.sort(() => Math.random() - 0.5)
+  const domanda = questions[index];
+  questionElement.innerHTML = `<h1>${domanda.question}</h1>`;
+  contatoreElement.textContent = `${index + 1}`;
+  const tutteLeRisposte = [
+    ...domanda.incorrect_answers,
+    domanda.correct_answer,
+  ];
+  tutteLeRisposte.sort(() => Math.random() - 0.5);
   tutteLeRisposte.forEach((risposta, rispostaIndex) => {
-    const bottone = document.createElement("button")
-    bottone.textContent = risposta
-    bottone.classList.add("button-ans")
-    risposteElement.appendChild(bottone)
+    const bottone = document.createElement("button");
+    bottone.textContent = risposta;
+    bottone.classList.add("button-ans");
+    risposteElement.appendChild(bottone);
     bottone.addEventListener("click", () => {
-      let isCorrect = risposta === domanda.correct_answer
+      let isCorrect = risposta === domanda.correct_answer;
       //modifica background colore bottone in base alla risposta
       if (isCorrect) {
-        bottone.classList.add("green")
+        bottone.classList.add("green");
       } else {
-        bottone.classList.add("red")
+        bottone.classList.add("red");
       }
 
-      console.log(isCorrect ? "Risposta corretta!" : "Risposta sbagliata!")
+      console.log(isCorrect ? "Risposta corretta!" : "Risposta sbagliata!");
       //dove vengono push i dati inerenti alle risposte
       results.push({
         question: domanda.question,
         selectedAnswer: risposta,
         correctAnswer: domanda.correct_answer,
         isCorrect: isCorrect,
-      })
-      index++
+      });
+      index++;
       // Controlla se siamo all'ultima domanda
       if (index === questions.length) {
         // Se siamo all'ultima domanda, aggiungi il pulsante per mostrare i risultati
-        const footer = document.querySelector("footer")
-        const showResultButton = document.createElement("button")
-        showResultButton.textContent = "MOSTRA RISULTATO TEST"
-        showResultButton.id = "show-result-button"
-        showResultButton.classList.add("button-ans")
-        footer.innerHTML = "" // Rimuovi eventuali vecchi pulsanti nel footer
-        footer.appendChild(showResultButton)
+        const footer = document.querySelector("footer");
+        const showResultButton = document.createElement("button");
+        showResultButton.textContent = "MOSTRA RISULTATO TEST";
+        showResultButton.id = "show-result-button";
+        showResultButton.classList.add("button-ans");
+        footer.innerHTML = ""; // Rimuovi eventuali vecchi pulsanti nel footer
+        footer.appendChild(showResultButton);
         showResultButton.addEventListener("click", () => {
-          console.log("Hai completato tutte le domande!")
-          window.location.href = "result.html" // Redirigi verso la pagina dei risultati
-        })
+          console.log("Hai completato tutte le domande!");
+          window.location.href = "result.html"; // Redirigi verso la pagina dei risultati
+        });
         // Salva l'array results nei dati di sessione dopo che l'utente ha completato il quiz
-        sessionStorage.setItem("results", JSON.stringify(results))
+        sessionStorage.setItem("results", JSON.stringify(results));
       } else {
         // Se non siamo all'ultima domanda, mostra la prossima domanda
-        setTimeout(nextQuestion, 1000)
+        setTimeout(nextQuestion, 100);
       }
-    })
-  })
+    });
+  });
 }
 
 // Chiama la funzione startTimer per avviare il quiz
-startTimer()
-nextQuestion()
-console.log("Array da riempire", results)
+startTimer();
+nextQuestion();
+console.log("Array da riempire", results);
