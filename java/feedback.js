9625ec8b06ba6dda47f53c05372581267e4a94d9
ABBA3124 <davideabbadessa73@gmail.com>
2024-03-21 21:47:56 +0100

    fix completo di feedback page
    finalmente ho finito tutto, ho fixato anche la madonna, è perfettamente funzionante e pronto da far vedere al grandissimo MICELI <3

--- java/feedback.js
+++ java/feedback.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
   const imageContainer = document.getElementById('imageContainer')
   const moreInfoButton = document.getElementById('moreInfoButton')
   const submitButton = document.getElementById('submitButton')
+  const feedbackTextSection = document.querySelector('.feedbackText')
 
   function handleClick(index) {
     for (let i = 0; i <= index; i++) {
@@ -27,7 +28,6 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   }
 
-  // Aggiunge un ascoltatore per il click sul pulsante "Submit"
   submitButton.addEventListener('click', submitFeedback)
 
   // Funzione per gestire l'invio del feedback
@@ -37,54 +37,42 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const selectedStars = document.querySelectorAll('.stars img.light')
 
-    if (selectedStars.length >= 6) {
-      // Nasconde le stelline e la sezione del commento dopo il submit
-      document.querySelector('.stars').style.display = 'none'
-      document.querySelector('.commentArea').style.display = 'none'
-      submitButton.style.display = 'none'
+    // Nasconde le stelline, la sezione del commento e il tasto submit dopo il submit
+    document.querySelector('.stars').style.display = 'none'
+    document.querySelector('.commentArea').style.display = 'none'
+    feedbackTextSection.style.display = 'none' // Nasconde il testo di feedback
+    submitButton.style.display = 'none' // Nasconde il pulsante "Submit"
 
-      // Mostra l'immagine dopo il submit solo se più di 6 stelline sono state selezionate
+    if (selectedStars.length >= 6) {
+      // Mostra il messaggio di ringraziamento
+      feedbackMessage.innerText = 'Thanks for your feedback!'
       imageContainer.innerHTML = `
         <div id="gifContainer" style="display: flex; justify-content: center; align-items: center;">
           <img src="https://media1.tenor.com/m/6vWYnc0dkisAAAAd/your-feedback-is-appreciated-we-value-your-feedback.gif" alt="Love GIF" style="max-width: 100%;">
         </div>`
-      imageContainer.style.display = 'block'
-
-      // Mostra il messaggio di ringraziamento
-      feedbackMessage.innerText = 'Thanks for your feedback'
     } else {
-      // Nasconde le stelline, la sezione del commento e il tasto submit dopo il submit
-      document.querySelector('.stars').style.display = 'none'
-      document.querySelector('.commentArea').style.display = 'none'
-      submitButton.style.display = 'none'
-
-      // Mostra l'immagine di tristezza e il messaggio
+      // Mostra il messaggio di scuse
+      feedbackMessage.innerText = "We're sorry to hear your feedback."
       imageContainer.innerHTML = `
         <div id="gifContainer" style="display: flex; justify-content: center; align-items: center;">
           <img src="https://media1.tenor.com/m/9rRu1_qcVpIAAAAC/shredder-mr-burns.gif" alt="Sad GIF" style="max-width: 100%;">
         </div>`
-      imageContainer.style.display = 'block'
-
-      feedbackMessage.innerText =
-        'Sorry, please let us know how we can improve.'
     }
 
-    // Aggiunge il bottone "More Info" al container dell'immagine
     moreInfoButton.style.display = 'block'
     moreInfoButton.style.margin = '0 auto'
     moreInfoButton.style.marginBottom = '20px'
+    moreInfoButton.style.marginTop = '50px'
+
     imageContainer.appendChild(moreInfoButton)
   }
 
-  // Aggiunge un ascoltatore per il click sul bottone "More Info"
   moreInfoButton.addEventListener('click', redirectToMoreInfoPage)
 
   function redirectToMoreInfoPage() {
-    // Modifica l'URL per il reindirizzamento
     window.location.href = 'https://epicode.com/it/'
   }
 
-  // Aggiunge un ascoltatore per il click sulle stelle
   stars.forEach((star, index) => {
     star.addEventListener('click', () => {
       handleClick(index)
